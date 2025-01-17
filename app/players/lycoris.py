fc24129ab92173ddd8a95a25c6ef1aeb4097bc5b
skoruppa <skoruppa@gmail.com>
2025-01-17 02:41:32 +0100

    Handling both url types in lycoris

--- app/players/lycoris.py
+++ app/players/lycoris.py
@@ -1,4 +1,5 @@
 import logging
+from urllib.parse import quote, urlencode
 
 import aiohttp
 import base64
@@ -9,6 +10,7 @@ from app.routes.utils import get_random_agent
 
 headers = {"User-Agent": get_random_agent()}
 GET_SECONDARY_URL = "https://www.lycoris.cafe/api/getSecondaryLink"
+GET_THIRD_URL = "https://www.lycoris.cafe/api/getLink"
 
 
 def decode_video_links(encoded_url):
@@ -30,21 +32,40 @@ def decode_video_links(encoded_url):
         )
 
         # Decode base64
-        return json.loads(base64.b64decode(decoded).decode('utf-8'))
+        base64_decoded = base64.b64decode(decoded).decode('utf-8')
+        try:
+            data = json.loads(base64_decoded)  # Próba załadowania ciągu jako JSON
+            return data  # Jeśli nie wystąpi wyjątek, to JSON jest poprawny
+        except json.JSONDecodeError:
+            return base64_decoded
     except Exception as error:
         print(f"Error decoding URL: {error}")
         return None
 
 
-async def fetch_and_decode(session: aiohttp.ClientSession, episode_id: str):
+async def fetch_and_decode_video(session: aiohttp.ClientSession, episode_id: str, is_secondary: bool = False):
+    """
+    Pobiera dane wideo z odpowiedniego URL na podstawie parametru is_secondary.
+    Jeśli is_secondary jest True, konwertuje link do odpowiedniego formatu.
+    """
     try:
-        async with session.get(GET_SECONDARY_URL, params={"id": episode_id}, headers=headers) as response:
+        if is_secondary:
+            # Przekształć URL (analogicznie do funkcji fetch_and_decode_second)
+            converted_text = bytes(episode_id, "utf-8").decode("unicode_escape")
+            final_text = converted_text.encode("latin1").decode("utf-8")
+            params = {"link": final_text}
+            url = GET_THIRD_URL
+        else:
+            params = {"id": episode_id}
+            url = GET_SECONDARY_URL
+
+        async with session.get(url, params=params, headers=headers) as response:
             response.raise_for_status()
             data = await response.text()
-            decoded_url = decode_video_links(data)
-            return decoded_url
+            return decode_video_links(data)
+
     except aiohttp.ClientError as e:
-        print(f"Error during request: {e}")
+        logging.error(f"Error during request: {e}")
         return None
 
 
@@ -84,10 +105,31 @@ async def get_video_from_lycoris_player(url: str):
                     if match:
                         episode_data = match.group(1)
                         match = re.search(r'id\s*:\s*(\d+)', episode_data)
+                        streams_data = re.findall(r'(FHD|HD|SD):"([^"]+)"', episode_data)
+
+                        # Przyjmujemy kolejność FHD > HD > SD
+                        qualities = {'FHD': None, 'HD': None, 'SD': None}
+
+                        for stream_data in streams_data:
+                            quality, value = stream_data
+                            if qualities[quality] is None:
+                                qualities[quality] = value
+
+                        # Wybieramy najwyższą jakość
+                        highest_quality = None
+                        if qualities['FHD']:
+                            highest_quality = {"url": qualities['FHD'], 'quality': 1080}
+                        elif qualities['HD']:
+                            highest_quality = {"url": qualities['HD'], 'quality': 720}
+                        elif qualities['SD']:
+                            highest_quality = {"url": qualities['SD'], 'quality': 480}
                         if match:
                             episode_id = match.group(1)
-                            video_links = await fetch_and_decode(session, episode_id)
-                            if video_links:
+                            video_links = await fetch_and_decode_video(session, episode_id, is_secondary=False)
+                            if not video_links:
+                                video_link = await fetch_and_decode_video(session, highest_quality['url'], is_secondary=True)
+                                return video_link, highest_quality['quality']
+                            else:
                                 return get_highest_quality(video_links)
 
         return None
