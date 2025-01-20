377569a370e6507266393bc02017fe605c8987d7
skoruppa <skoruppa@gmail.com>
2025-01-20 01:17:12 +0100

    Rebased config

---
+++ config.py
@@ -0,0 +1,28 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Config:
+    """
+    Configuration class
+    """
+    FLASK_HOST = os.getenv('FLASK_RUN_HOST', "localhost")
+    FLASK_PORT = os.getenv('FLASK_RUN_PORT', "5000")
+    PROXIFY_CDA = os.getenv('PROXIFY_CDA', True)
+    CDA_PROXY_URL = os.getenv('CDA_PROXY_URL', "")  # MediaFlow Proxy
+    CDA_PROXY_PASSWORD = os.getenv('CDA_PROXY_PASSWORD', "")  # MediaFlowProxy API_PASSWORD
+    KITSU_STREMIO_API_URL = os.getenv('KITSU_STREMIO_API_URL', "https://anime-kitsu.strem.fun/meta")
+
+    DEBUG = os.getenv('FLASK_DEBUG', False)
+    DATABASE = "/tmp/database.json"
+
+    # Env dependent configs
+    if DEBUG in ["1", True, "True"]:  # Local development
+        PROTOCOL = "http"
+        REDIRECT_URL = f"{FLASK_HOST}:{FLASK_PORT}"
+    else:  # Production environment
+        PROTOCOL = "https"
+        REDIRECT_URL = f"{FLASK_HOST}"
