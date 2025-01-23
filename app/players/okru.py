513dc3cac6a68572fc9094339dc00ecd6697fd40
janowakps <36198623+janowakps@users.noreply.github.com>
2025-01-23 07:17:15 +0100

    Fix okru.py

--- app/players/okru.py
+++ app/players/okru.py
@@ -2,7 +2,7 @@ import aiohttp
 from bs4 import BeautifulSoup
 from app.routes.utils import get_random_agent
 import json
-
+from flask import request
 
 def fix_quality(quality):
     quality_mapping = {
@@ -22,7 +22,7 @@ def extract_link(item, attr):
     return item.get(attr).replace("\\\\u0026", "&")
 
 
-def videos_from_json(video_json):
+def videos_from_json(video_json, user_agent):
     videos = []
     for item in video_json:
         video_url = extract_link(item, 'url')
@@ -33,16 +33,29 @@ def videos_from_json(video_json):
             videos.append({'url': video_url, 'quality': quality})
     highest_quality_video = max(videos, key=lambda x: x['quality'])
 
-    video_headers = {"response": {
-        "Access-Control-Allow-Origin": "*",
-        "Content-Type": "video/mp4"
-    }}
+    video_headers = {
+        "request": {
+            "User-Agent": user_agent
+        },
+        "response": {
+            "Access-Control-Allow-Origin": "*",
+            "Content-Type": "video/mp4"
+        }
+    }
 
-    return highest_quality_video['url'], f'{highest_quality_video['quality']}p', video_headers
+    return highest_quality_video['url'], f"{highest_quality_video['quality']}p", video_headers
 
 
 async def get_video_from_okru_player(url):
-    headers = {"User-Agent": get_random_agent()}
+    referer = request.headers.get('Referer', None)
+    user_agent = request.headers.get('User-Agent', None)
+
+    if referer and "web.stremio.com" in referer:
+        headers = {"User-Agent": user_agent}
+    else:
+        user_agent = get_random_agent()
+        headers = {"User-Agent": user_agent}
+
     async with aiohttp.ClientSession() as session:
         async with session.get(url, headers=headers) as response:
             text = await response.text()
@@ -62,4 +75,4 @@ async def get_video_from_okru_player(url):
         playlist_url = extract_link(next((video for video in video_json if video['name'] == 'ondemandDash'), None), "ondemandDash")
         return playlist_url, 'unknown'
     else:
-        return videos_from_json(video_json)
+        return videos_from_json(video_json, user_agent)
