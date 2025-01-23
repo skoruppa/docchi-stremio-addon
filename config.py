a9ceb645126803fcd22dc6c19b756104d9cd2a17
skoruppa <skoruppa@gmail.com>
2025-01-24 00:49:20 +0100

    Update config.py

--- config.py
+++ config.py
@@ -13,16 +13,16 @@ class Config:
     FLASK_PORT = os.getenv('FLASK_RUN_PORT', "5000")
     CACHE_TYPE = 'SimpleCache'
     CACHE_DEFAULT_TIMEOUT = 600
-    PROXIFY_CDA = os.getenv('PROXIFY_CDA', True)
-    CDA_PROXY_URL = os.getenv('CDA_PROXY_URL', "")  # MediaFlow Proxy
-    CDA_PROXY_PASSWORD = os.getenv('CDA_PROXY_PASSWORD', "")  # MediaFlowProxy API_PASSWORD
+    PROXIFY_STREAMS = os.getenv('PROXIFY_STREAMS', False)  # proxify needed streams
+    STREAM_PROXY_URL = os.getenv('STREAM_PROXY_URL', "")  # MediaFlow Proxy
+    STREAM_PROXY_PASSWORD = os.getenv('STREAM_PROXY_PASSWORD', "")  # MediaFlowProxy API_PASSWORD
     KITSU_STREMIO_API_URL = os.getenv('KITSU_STREMIO_API_URL', "https://anime-kitsu.strem.fun/meta")
 
-    DEBUG = os.getenv('FLASK_DEBUG', False)
+    DEBUG = os.getenv('FLASK_DEBUG', True)
     DATABASE = "/tmp/database.json"
 
     # Env dependent configs
-    if DEBUG in ["1", True, "True"]:  # Local development
+    if DEBUG in ["1", True, "False"]:  # Local development
         PROTOCOL = "http"
         REDIRECT_URL = f"{FLASK_HOST}:{FLASK_PORT}"
     else:  # Production environment
