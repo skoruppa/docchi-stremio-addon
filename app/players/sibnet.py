9d192b7e1551cb9e2942c1a2be3107090297634a
janowakps <36198623+janowakps@users.noreply.github.com>
2025-01-24 13:08:44 +0100

    Update sibnet.py
    fix for web.stremio version

--- app/players/sibnet.py
+++ app/players/sibnet.py
@@ -1,10 +1,10 @@
 import aiohttp
 from bs4 import BeautifulSoup
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urljoin
+from app.routes.utils import get_random_agent
 
 
 async def get_video_from_sibnet_player(url: str) -> tuple:
-
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
             if response.status != 200:
@@ -25,9 +25,12 @@ async def get_video_from_sibnet_player(url: str) -> tuple:
                 .split('"', 2)[1]
             )
 
-            video_headers = {"request": {
-                "Referer": url,
-            }}
+            video_headers = {
+                "request": {
+                    "Referer": url,
+                    "User-Agent": get_random_agent()
+                }
+            }
 
             if "http" in slug:
                 video_url = slug
@@ -35,4 +38,11 @@ async def get_video_from_sibnet_player(url: str) -> tuple:
                 host = urlparse(url).netloc
                 video_url = f"https://{host}{slug}"
 
-    return video_url, 'unknown', video_headers
+            async with session.head(video_url, headers=video_headers["request"]) as head_response:
+                if head_response.status in (301, 302) and "Location" in head_response.headers:
+                    location = head_response.headers["Location"]
+                    if not location.startswith("http"):
+                        location = urljoin(f"https://{host}", location)
+                    video_url = location
+
+    return video_url, "unknown", video_headers
