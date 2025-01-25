c336f09f03aeff86addbe7ecd0ff4d8ab4621e64
skoruppa <skoruppa@gmail.com>
2025-01-25 01:50:35 +0100

    Move both requests to one session

--- app/players/lulustream.py
+++ app/players/lulustream.py
@@ -53,11 +53,11 @@ def fix_m3u8_link(link: str) -> str:
     return fixed_link
 
 
-async def fetch_resolution_from_m3u8(m3u8_url, headers):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(m3u8_url, headers=headers, timeout=10) as response:
-            response.raise_for_status()
-            m3u8_content = await response.text()
+async def fetch_resolution_from_m3u8(session, m3u8_url, headers):
+    m3u8_url = m3u8_url
+    async with session.get(m3u8_url, headers=headers, timeout=10) as response:
+        response.raise_for_status()
+        m3u8_content = await response.text()
 
     resolution_match = re.search(r'RESOLUTION=\d+x(\d+)', m3u8_content)
 
@@ -78,17 +78,17 @@ async def get_video_from_lulustream_player(filelink):
             response.raise_for_status()
             html_content = await response.text()
 
-    player_data = ""
-    if re.search(r"eval\(function\(p,a,c,k,e", html_content):
-        player_data = unpack(html_content)
+        player_data = ""
+        if re.search(r"eval\(function\(p,a,c,k,e", html_content):
+            player_data = unpack(html_content)
 
-    m3u8_match = re.search(r"sources:\[\{file:\"([^\"]+)\"", player_data)
-    if not m3u8_match:
-        return None, None
+        m3u8_match = re.search(r"sources:\[\{file:\"([^\"]+)\"", player_data)
+        if not m3u8_match:
+            return None, None
 
-    stream_url = fix_m3u8_link(m3u8_match.group(1))
-    quality = await fetch_resolution_from_m3u8(stream_url, headers)
+        stream_url = fix_m3u8_link(m3u8_match.group(1))
+        quality = await fetch_resolution_from_m3u8(session, stream_url, headers)
 
-    stream_headers = {'request': headers}
+        stream_headers = {'request': headers}
 
-    return stream_url, f'{quality}p', stream_headers
+        return stream_url, f'{quality}p', stream_headers
