eca78f3b94b4e47d57c87b50ea5695e75c2adc9a
skoruppa <skoruppa@gmail.com>
2025-01-27 01:10:18 +0100

    Move into session

--- app/players/streamtape.py
+++ app/players/streamtape.py
@@ -32,37 +32,37 @@ async def get_video_from_streamtape_player(url: str):
                 return None, None, None
             content = await response.text()
 
-    soup = BeautifulSoup(content, 'html.parser')
-    target_line = "document.getElementById('robotlink')"
-    script_tag = soup.find("script", string=lambda text: target_line in text if text else False)
-    if not script_tag or not script_tag.string:
-        return None, None, None
+            soup = BeautifulSoup(content, 'html.parser')
+            target_line = "document.getElementById('robotlink')"
+            script_tag = soup.find("script", string=lambda text: target_line in text if text else False)
+            if not script_tag or not script_tag.string:
+                return None, None, None
 
-    script_content = script_tag.string
-    first_part = re.search(r'innerHTML = "(.*?)"', script_content)
+            script_content = script_tag.string
+            first_part = re.search(r'innerHTML = "(.*?)"', script_content)
 
-    # Wyszukaj drugi ciąg zaczynający się od "xcd" i zignoruj początkowy "xcd"
-    second_part = re.findall(r'\(\'xcd(.*?)\'\)', script_content)
-    stream_data = first_part.group(1)[:-1] + second_part[1]
+            # Wyszukaj drugi ciąg zaczynający się od "xcd" i zignoruj początkowy "xcd"
+            second_part = re.findall(r'\(\'xcd(.*?)\'\)', script_content)
+            stream_data = first_part.group(1)[:-1] + second_part[1]
 
-    video_headers = None
+            video_headers = None
 
-    stream_url = f'https:/{stream_data}&stream=1'
-    if PROXIFY_STREAMS:
-        post_data = {
-            "mediaflow_proxy_url": STREAM_PROXY_URL,
-            "endpoint": "/proxy/stream",
-            "destination_url": stream_url,
-            "expiration": 7200,
-            "request_headers": headers,
-            "api_password": STREAM_PROXY_PASSWORD,
-        }
-        async with session.post(f'{STREAM_PROXY_URL}/generate_encrypted_or_encoded_url',
-                                json=post_data) as response:
-            response.raise_for_status()
-            result = await response.json()
-        stream_url = result.get("encoded_url", {})
-    else:
-        video_headers = {'request': headers}
+            stream_url = f'https:/{stream_data}&stream=1'
+            if PROXIFY_STREAMS:
+                post_data = {
+                    "mediaflow_proxy_url": STREAM_PROXY_URL,
+                    "endpoint": "/proxy/stream",
+                    "destination_url": stream_url,
+                    "expiration": 7200,
+                    "request_headers": headers,
+                    "api_password": STREAM_PROXY_PASSWORD,
+                }
+                async with session.post(f'{STREAM_PROXY_URL}/generate_encrypted_or_encoded_url',
+                                        json=post_data) as response:
+                    response.raise_for_status()
+                    result = await response.json()
+                stream_url = result.get("encoded_url", {})
+            else:
+                video_headers = {'request': headers}
 
-    return stream_url, quality, video_headers
+            return stream_url, quality, video_headers
