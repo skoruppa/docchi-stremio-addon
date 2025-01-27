90b326de5429b8cf261db82aaef5f700bd5b7e5f
skoruppa <skoruppa@gmail.com>
2025-01-27 01:08:04 +0100

    Proxify Streamtape

--- app/players/streamtape.py
+++ app/players/streamtape.py
@@ -2,7 +2,11 @@ import aiohttp
 import re
 from bs4 import BeautifulSoup
 from app.routes.utils import get_random_agent
+from config import Config
 
+PROXIFY_STREAMS = Config.PROXIFY_STREAMS
+STREAM_PROXY_URL = Config.STREAM_PROXY_URL
+STREAM_PROXY_PASSWORD = Config.STREAM_PROXY_PASSWORD
 
 async def get_video_from_streamtape_player(url: str):
     quality = "unknown"
@@ -39,11 +43,26 @@ async def get_video_from_streamtape_player(url: str):
 
     # Wyszukaj drugi ciąg zaczynający się od "xcd" i zignoruj początkowy "xcd"
     second_part = re.findall(r'\(\'xcd(.*?)\'\)', script_content)
-
     stream_data = first_part.group(1)[:-1] + second_part[1]
 
-    video_url = f'https:/{stream_data}&stream=1'
+    video_headers = None
 
-    stream_headers = {'request': headers}
+    stream_url = f'https:/{stream_data}&stream=1'
+    if PROXIFY_STREAMS:
+        post_data = {
+            "mediaflow_proxy_url": STREAM_PROXY_URL,
+            "endpoint": "/proxy/stream",
+            "destination_url": stream_url,
+            "expiration": 7200,
+            "request_headers": headers,
+            "api_password": STREAM_PROXY_PASSWORD,
+        }
+        async with session.post(f'{STREAM_PROXY_URL}/generate_encrypted_or_encoded_url',
+                                json=post_data) as response:
+            response.raise_for_status()
+            result = await response.json()
+        stream_url = result.get("encoded_url", {})
+    else:
+        video_headers = {'request': headers}
 
-    return video_url, quality, stream_headers
+    return stream_url, quality, video_headers
