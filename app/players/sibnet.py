f22c2e1507004282ff1fb8396cb3a720a70a27c0
skoruppa <skoruppa@gmail.com>
2025-02-17 11:33:51 +0100

    Update sibnet.py

--- app/players/sibnet.py
+++ app/players/sibnet.py
@@ -15,7 +15,7 @@ async def get_video_from_sibnet_player(url: str) -> tuple:
     }
     if PROXIFY_STREAMS:
         url = f'{STREAM_PROXY_URL}/proxy/stream?d={url}&api_password={STREAM_PROXY_PASSWORD}'
-    async with aiohttp.ClientSession() as session:
+    async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(verify_ssl=False)) as session:
         async with session.get(url, headers=headers) as response:
             if response.status != 200:
                 print(f'Wrong Status: {response.text}')
