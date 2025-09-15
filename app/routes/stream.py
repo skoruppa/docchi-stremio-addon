9b65ad04e772f7a0977ce5151cc7138b89a01293
skoruppa <skoruppa@gmail.com>
2025-09-15 22:08:10 +0200

    Disable abyss
    Abyss retusn a binary playlist that can't be supported by Stremio

--- app/routes/stream.py
+++ app/routes/stream.py
@@ -28,7 +28,7 @@ from app.players.mp4upload import get_video_from_mp4upload_player
 from app.players.earnvid import get_video_from_earnvid_player
 from app.players.filemoon import get_video_from_filemoon_player
 from app.players.streamup import get_video_from_streamup_player
-from app.players.abyss import get_video_from_abyss_player
+# from app.players.abyss import get_video_from_abyss_player  #was fun, but can't support their binary playlist
 
 from config import Config
 
@@ -36,7 +36,7 @@ stream_bp = Blueprint('stream', __name__)
 PROXIFY_STREAMS = Config.PROXIFY_STREAMS
 supported_streams = ['cda', 'lycoris.cafe', 'ok', 'sibnet', 'dailymotion', 'vk', 'gdrive', 'google drive', 'uqload',
                      'lulustream', 'streamtape', 'rumble', 'default', 'vidtube', 'upn', 'upns', 'rpm', 'rpmhub',
-                     'mp4upload', 'filemoon', 'earnvid', 'streamup', 'abyss']
+                     'mp4upload', 'filemoon', 'earnvid', 'streamup']
 
 
 async def process_player(player):
@@ -96,8 +96,6 @@ async def process_player(player):
         url, quality, headers = await get_video_from_filemoon_player(player['player'])
     elif player_hosting == 'streamup':
         url, quality, headers = await get_video_from_streamup_player(player['player'])
-    elif player_hosting == 'abyss':
-        url, quality, headers = await get_video_from_abyss_player(player['player'])
 
     stream.update({'url': url, 'quality': quality, 'headers': headers, 'inverted': inverted})
     return stream
